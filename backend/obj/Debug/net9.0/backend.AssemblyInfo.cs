d0686cb933d2b5d8d578882d343c45af5ca3d4a3
kai-lucky72 <kagabolucky72@gmail.com>
2025-08-14 08:50:01 +0200

    small changes

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02d71647f4abd056a0a2cc2cf6eedd6ad6fe8ca9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8139752ff3044cb015e63553a871656fc256b4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
